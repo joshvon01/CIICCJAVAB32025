775a2f36563204ee241bff39d5a28df67a5d10c5
joshvon01 <joshvon.0@gmail.com>
2025-07-08 04:14:33 +0000

    Done

--- Chapter1/TaskSheet111.java
+++ Chapter1/TaskSheet111.java
@@ -1,5 +1,9 @@
-public class TaskSheet111 {
-    public static void main(String[] args){
-        System.out.println("Kamusta Mundo!");
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class TaskSheet111 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
