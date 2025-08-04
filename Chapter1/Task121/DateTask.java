571f2a9f5445dcb1230591fd6f55685be6031ce8
joshvon01 <joshvon.0@gmail.com>
2025-08-04 03:41:55 +0000

    DONE

--- Chapter1/Task121/DateTask.java
+++ Chapter1/Task121/DateTask.java
@@ -13,8 +13,9 @@ public class DateTask {
         setDate(m, d, y);
     }
 
+    @Override
     public String toString() {
-        return month + "-" + day + "-" + year;
+        return this.month + "/" + this.day + "/" + this.year;
     }
 
     public void setDate(int m, int d, int y) {
@@ -23,6 +24,7 @@ public class DateTask {
             this.year = (short) y;
             this.month = (byte) m;
         } else {
+            System.out.println("Attempting to create a non-valid date: " + m + "/" + d + "/" + y);
             this.day = (byte) 0;
             this.year = (short) 0;
             this.month = (byte) 0;
@@ -30,55 +32,56 @@ public class DateTask {
     }
 
     public static void leapYears() {
-        System.out.println("Leap Years between 1980 and 2023:");
-        for (int i = 1980; i <= 2023; i = i + 4) {
+        System.out.println("\n--- Leap Years between 1980 and 2023 ---");
+        for (int i = 1980; i <= 2023; i++) {
             if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0)) {
-                System.out.println("The year " + i + " is a leap year");
+                System.out.println("The year " + i + " is a leap year.");
             }
         }
+        System.out.println("----------------------------------------");
     }
 
     public int getDay() {
-        return day;
+        return this.day;
     }
 
     public void setDay(int day) {
         if (valid(day, this.month, this.year)) {
             this.day = (byte) day;
         } else {
-            this.day = (byte) 0;
+            System.out.println("Invalid day entered (" + day + "). Day not changed.");
         }
     }
 
     public int getMonth() {
-        return month;
+        return this.month;
     }
 
     public void setMonth(int month) {
         if (valid(this.day, month, this.year)) {
             this.month = (byte) month;
         } else {
-            this.month = (byte) 0;
+            System.out.println("Invalid month entered (" + month + "). Month not changed.");
         }
     }
 
     public int getYear() {
-        return year;
+        return this.year;
     }
 
     public void setYear(int year) {
         if (valid(this.day, this.month, year)) {
             this.year = (short) year;
         } else {
-            this.year = (short) 0;
+            System.out.println("Invalid year entered (" + year + "). Year not changed.");
         }
     }
 
     private boolean valid(int day, int month, int year) {
         if (day > 31 || day < 1 || month > 12 || month < 1 || year < 1) {
-            System.out.println("Attempting to create a non-valid date " + month + "/" + day + "/" + year);
             return false;
         }
+
         switch (month) {
             case 4:
             case 6:
@@ -86,9 +89,13 @@ public class DateTask {
             case 11:
                 return (day <= 30);
             case 2:
-                return day <= 28 || (day == 29 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)));
-            default:
-                return true;
+                boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+                if (isLeapYear) {
+                    return day <= 29;
+                } else {
+                    return day <= 28;
+                }
         }
+        return true;
     }
 }
--- Chapter1/Task121/MyDate.java
+++ Chapter1/Task121/MyDate.java
@@ -1,15 +1,11 @@
 package Task121;
 
-//need to fix import
-import Task121.DateTask;
-
 public class MyDate {
     public static void main(String[] args) {
         DateTask date1 = new DateTask();
         DateTask date2 = new DateTask();
 
         date1.setDate(1, 1, 1978);
-
         date2.setDate(9, 21, 1984);
 
         System.out.println("Date 1: " + date1.toString());
