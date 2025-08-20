5c82afedea4bfbd14ada0c440ac594a6279c688f
joshvon01 <joshvon.0@gmail.com>
2025-08-20 05:54:38 +0000

    DONE

--- Chapter4/Task10.java
+++ Chapter4/Task10.java
@@ -15,7 +15,7 @@ class Student {
 public class Task10 {
     public static void main(String[] args) {
         Student[] students = new Student[] {
-            new Student("Morgan", "Freeman"),
+            new Student("Morganaa", "Freeman"),
             new Student("Brad", "Pitt"),
             new Student("Kevin", "Spaceyaa"),
         };
